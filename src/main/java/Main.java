251c3ee1dd77303172f80339079a8b057fd14e36
Elena <reikon9@rambler.ru>
2020-07-08 22:43:23 +0300

    add homework to lesson 6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,20 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        int[] myArr = {1, 4, 4,  4, 1, 7};
+        int[] myArr1 = {1, 4, 4,  4, 1, 7};
+        int[] arr = {4, 4,4,4,4};
+        int[] a = {1,1,1,1,1};
+        int[] aaa = {1, 1, 4, 3};
+
+
+        WorkWithArrays ar = new WorkWithArrays();
+
+        System.out.println(ar.method_2(aaa, 1, 4));
+
+//        System.out.println(Arrays.toString(ar.method_1(myArr, 4)));
     }
 }
--- src/test/java/MassTestMethod_2.java
+++ src/test/java/MassTestMethod_2.java
@@ -1,8 +1,51 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 @RunWith(Parameterized.class)
 
 public class MassTestMethod_2 {
+    private int[] arr;
+    private int value1;
+    private int value2;
+    private boolean b;
+
+    WorkWithArrays wwa;
+
+    public MassTestMethod_2(int[] arr, int value1, int value2, boolean b) {
+        this.arr = arr;
+        this.value1 = value1;
+        this.value2 = value2;
+        this.b = b;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {new int[]{1, 1, 1, 4, 4, 1, 4, 4}, 1, 4, true},
+                {new int[]{1, 1, 1, 1, 1, 1},  1, 4, false},
+                {new int[]{4, 4, 4, 4}, 1, 4, false},
+                {new int[]{1, 4, 4, 1, 1, 4, 3}, 1, 4, false},
+        });
+    }
+
+    @Before
+    public void init() {
+        wwa = new WorkWithArrays();
+    }
+
+    @Test
+    public void testAdd() {
+        Assert.assertEquals(wwa.method_2(arr, value1, value2), b);
+    }
+
+
+
+
 
 }
